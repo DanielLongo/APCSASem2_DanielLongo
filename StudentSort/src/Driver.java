d00856b6b4fab8563beeb75f1c7e8f2bf95c87fe
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2019-01-25 10:10:08 -0800

    completed

--- StudentSort/src/Driver.java
+++ StudentSort/src/Driver.java
@@ -1,17 +1,18 @@
 public class Driver {
     public static void main(String[] args) {
-        Student[] students = new Student[100];
+        Student[] students = new Student[10000];
         for (int i = 0; i < students.length; i++) {
             students[i] = new Student (true);
         }
         MergeSort sorter = new MergeSort ( );
         students = sorter.mergeSort (students, students.length);
         for (int i = 0; i < students.length; i++) {
-            System.out.printf ("Student id " + students[i].getStudentId () + " GPA: ");
-            System.out.println (students[i].getGPA ());
+//            System.out.printf ("Student id " + students[i].getStudentId () + " GPA: ");
+//            System.out.println (students[i].getGPA ());
 
 
         }
+        System.out.println ("Student with a 4.0 " + students[FindStudent.findStudent (students, 4.0)].getStudentId ());
         System.out.println ("Finished");
     }
 }
--- StudentSort/src/FindStudent.java
+++ StudentSort/src/FindStudent.java
@@ -1,9 +1,19 @@
 public class FindStudent {
-    public static int findStudent(Student [] x, int target) {
-        double value = -9999;
+    public static int findStudent(Student [] x, double target) {
         int index = x.length / 2;
-        while (value != target) {
+        int old = 0;
+        while (old != index) {
+            if (x[index].getGPA () == target) return index;
+            else if (x[index].getGPA () > target) {
+                old = index;
+                index = index - ((x.length - index)/2);
+            }
+            else {
+                old = index;
+                index = index + ((x.length - index)/2);
 
+            }
         }
+        return -1;
     }
 }
