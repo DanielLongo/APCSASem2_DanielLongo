f96c6481f51804a09cccfcc76cc398223dd9d967
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2019-04-11 09:18:04 -0700

    finished climbing club

---
+++ hw/src/climbingClub.java
@@ -0,0 +1,28 @@
+public class climbingClub {
+    private list<ClimbInfo> climbList;
+    public void addClimb(String peakName, int climbTime) {
+        climbInfo.add(new ClimbInfo(peakName, climbTime));
+    }
+
+    public void swap(int a,int  b) {
+        ClimbInfo temp = climbList.get(a);
+        climbList.set(b, a);
+        climbList.set(a, temp);
+
+    }
+    public void addClimbAlpha(String peakName, int climbTime) {
+        int targetIndex = 0;
+        climbList.add(new ClimbInfo(peakName, climbTime));
+        for (int i = 0; i < climbInfo.size
+            if (peakName.compareTo (climbInfo.get(i))) {
+                targetIndex = i
+            }
+            if (!peakName.compareTo (climbInfo.get(i))) {
+                break;
+            }
+        }
+        this.swap(climbList.size(), targetIndex)
+
+}
+//yes and no
+}
