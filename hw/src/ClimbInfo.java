1f16c4fad4756c91aabde5a1d87c1f6d13e5a86c
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2019-04-11 10:03:07 -0700

    finished

---
+++ hw/src/ClimbInfo.java
@@ -0,0 +1,10 @@
+public class ClimbInfo {
+    private String name;
+    private int climeTime;
+
+    ClimbInfo(String name, int climeTime) {
+        this.name = name;
+        this.climeTime = climeTime;
+    }
+
+}
